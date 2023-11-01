6c12c2abd00e0c0f8cea497a4f51840f92a0b83b
Natallia <natashamazur@bk.ru>
2023-11-01 17:45:27 +0300

    Initial Comment CardView isFaceUp

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Наталья Мазур on 1.11.23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
